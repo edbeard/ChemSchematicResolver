fe989a44131062fc9e66624ae2760c5b48a86b47
Edward Beard <ejb207@cam.ac.uk>
2019-04-11 11:01:46 +0100

    Updated R-group detection system tests
    Signed-off-by: Edward Beard <ejb207@cam.ac.uk>

--- tests/test_system.py
+++ tests/test_system.py
@@ -424,104 +424,107 @@ class TestSystem(unittest.TestCase):
         for x in gold:
             self.assertIn(x, labels)
 
-    def do_r_group(self, filename, filedir=examples_dir):
+    def do_r_group(self, filename, debug=False, filedir=examples_dir):
         """ Tests the R-group detection and recognition """
 
         test_diag = os.path.join(filedir, filename)
 
         # Read in float and raw pixel images
         fig = csde.io.imread(test_diag)
-        raw_fig = csde.io.imread(test_diag, raw=True)
+        fig_bbox = fig.get_bounding_box()
 
         # Create unreferenced binary copy
         bin_fig = copy.deepcopy(fig)
 
+        # Segment and classify diagrams and labels
         panels = csde.actions.segment(bin_fig)
         labels, diags = csde.actions.classify_kmeans(panels)
-        labels, diags = csde.actions.preprocessing(labels, diags, bin_fig)
+        labels, diags = csde.actions.preprocessing(labels, diags, fig)
 
         # Create output image
-        out_fig, ax = plt.subplots(figsize=(10, 6))
-        ax.imshow(fig.img)
+        if debug is True:
+            out_fig, ax = plt.subplots(figsize=(10, 6))
+            ax.imshow(fig.img)
+            colours = iter(
+                ['r', 'b', 'g', 'k', 'c', 'm', 'y', 'r', 'b', 'g', 'k', 'c', 'm', 'y', 'r', 'b', 'g', 'k', 'c', 'm',
+                 'y'])
 
-        labelled_diags = csde.actions.label_diags(diags, labels)
-
-        colours = iter(
-            ['r', 'b', 'g', 'k', 'c', 'm', 'y', 'r', 'b', 'g', 'k', 'c', 'm', 'y', 'r', 'b', 'g', 'k', 'c', 'm', 'y'])
+        labelled_diags = csde.actions.label_diags(labels, diags, fig_bbox)
 
         for diag in labelled_diags:
-            colour = next(colours)
-
-            diag_rect = mpatches.Rectangle((diag.left, diag.top), diag.width, diag.height,
-                                           fill=False, edgecolor=colour, linewidth=2)
-            ax.text(diag.left, diag.top + diag.height / 4, '[%s]' % diag.tag, size=diag.height / 20, color='r')
-            ax.add_patch(diag_rect)
 
             label = diag.label
-            label_rect = mpatches.Rectangle((label.left, label.top), label.width, label.height,
-                                            fill=False, edgecolor=colour, linewidth=2)
-            ax.text(label.left, label.top + label.height / 4, '[%s]' % label.tag, size=label.height / 5, color='r')
-            ax.add_patch(label_rect)
-
             diag.label = csde.actions.read_label(fig, label)
             diag = csde.r_group.detect_r_group(diag)
 
-            print(diag.label.r_group)
+            if debug is True:
 
+                colour = next(colours)
 
-            # label_strings = [label.text for label in label.text]
-            # output_label = ' '.join(label_strings)
-            # labels_text.append(output_label)
-            # print("Label %s : %s " % (label.tag, labels_text))
+                diag_rect = mpatches.Rectangle((diag.left, diag.top), diag.width, diag.height,
+                                               fill=False, edgecolor=colour, linewidth=2)
+                ax.text(diag.left, diag.top + diag.height / 4, '[%s]' % diag.tag, size=diag.height / 20, color='r')
+                ax.add_patch(diag_rect)
 
-        ax.set_axis_off()
-        plt.show()
+                label_rect = mpatches.Rectangle((label.left, label.top), label.width, label.height,
+                                                fill=False, edgecolor=colour, linewidth=2)
+                ax.text(label.left, label.top + label.height / 4, '[%s]' % label.tag, size=label.height / 5, color='r')
+                ax.add_patch(label_rect)
+
+                print(diag.label.r_group)
+
+        if debug is True:
+            ax.set_axis_off()
+            plt.show()
 
         return labelled_diags
 
-    # def test_ocr_all(self):
-    #
-    #     test_path = examples_dir
-    #     test_imgs = os.listdir(test_path)
-    #     for img_path in test_imgs:
-    #         self.do_ocr(img_path, filedir=test_path)
-    #
+    def test_r_group_detection_all(self):
+
+        test_path = examples_dir
+        test_imgs = os.listdir(test_path)
+        for img_path in test_imgs:
+            self.do_ocr(img_path, filedir=test_path)
+
     def test_r_group1(self):
         labelled_diags = self.do_r_group('S014372081630119X_gr1.jpg')
-        all_detected_r_groups_values = [token[1].text for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
-        gold = ['OCH3', 'CH3']
+        all_detected_r_groups_values = [(token[0].text, token[1].text)for diag in labelled_diags for tokens
+                                        in diag.label.r_group for token in tokens]
+        gold = [('R', 'OCH3'), ('R', 'CH3')]
         for x in gold:
             self.assertIn(x, all_detected_r_groups_values)
 
-        # TODO : Update all OCR tests to reflect new format
-
     def test_r_group2(self):
+        """ Tests included to check the rgroup variable is unpopulated"""
         labelled_diags = self.do_r_group('S014372081630122X_gr1.jpg')
         all_detected_r_groups_values = [token[1].text for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
         self.assertTrue(len(all_detected_r_groups_values) is 0)
-    #
-    # def test_ocr3(self):
-    #     labels_text = self.do_ocr('S014372081630167X_sc1.jpg')
-    #     gold = [[['PC71BM']], [['TPE-SQ']]]
-    #     for x in gold:
-    #         self.assertIn(x, labels_text)
-    #
 
-    # TODO : Fix repeated unit logic for this example
-    # def test_r_group4(self):
-    #     labelled_diags = self.do_r_group('S014372081730116X_gr8.jpg')
-    #     all_detected_r_groups_values = [token[1].text for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
-    #     gold = [[['J51']], [['PDBT-T1']], [['J61']], [['R=2-ethylhexyl'], ['PBDTTT-E-T']],
-    #             [['R=2-ethylhexyl'], ['PTB7-Th']],
-    #             [['R=2-ethylhexyl'], ['PBDB-T']], [['R=2-ethylhexyl'], ['PBDTTT-C-T']]]
-    #     for x in gold:
-    #         self.assertIn(x, all_detected_r_groups_values)
-    #
+    def test_r_group3(self):
+        labelled_diags = self.do_r_group('S014372081630167X_sc1.jpg')
+        all_detected_r_groups_values = [token[1].text for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
+        self.assertTrue(len(all_detected_r_groups_values) is 0)
+
+    def test_r_group4(self):
+        labelled_diags = self.do_r_group('S014372081730116X_gr8.jpg')
+        all_detected_r_groups_values = [tokens for diag in labelled_diags for tokens in diag.label.r_group]
+        unique_combos = []
+        for tokens in all_detected_r_groups_values:
+            tuple_set = set()
+            for token in tokens:
+                tuple_set.add((token[0].text, token[1].text))
+            unique_combos.append(tuple_set)
+
+        gold = {('R', '2-ethylhexyl')}  # All R-groups give this for this example
+        for diag in unique_combos:
+            self.assertEqual(diag, gold)
+
+        self.assertEqual(len(unique_combos), 4)
+
     def test_r_group5(self):
         labelled_diags = self.do_r_group('S0143720816300201_sc2.jpg')
         all_detected_r_groups_values = [token[1].text for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
         self.assertTrue(len(all_detected_r_groups_values) is 0)
-
 
     def test_r_group6(self):
         labelled_diags = self.do_r_group('S0143720816300274_gr1.jpg')
@@ -549,9 +552,10 @@ class TestSystem(unittest.TestCase):
         self.assertTrue(len(all_detected_r_groups_values) is 0)
 
     def test_r_group11(self):
+        # Currently failing : OCR performing poorly on semicolons
         labelled_diags = self.do_r_group('S0143720816301115_r75.jpg')
-        all_detected_r_groups_values = [token[1].text for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
-        gold = ['S', 'O']
+        all_detected_r_groups_values = [(token[0].text, token[1].text) for diag in labelled_diags for tokens in diag.label.r_group for token in tokens]
+        gold = [('X', 'S'), ('X', 'O')]
         for x in gold:
             self.assertIn(x, all_detected_r_groups_values)
 
@@ -560,18 +564,17 @@ class TestSystem(unittest.TestCase):
         all_detected_r_groups_values = [tokens for diag in labelled_diags for tokens in diag.label.r_group]
         unique_combos = []
         for tokens in all_detected_r_groups_values:
-            tuple_list = []
+            tuple_set = set()
             for token in tokens:
-                tuple_list.append((token[0].text, token[1].text))
-            unique_combos.append(tuple_list)
+                tuple_set.add((token[0].text, token[1].text))
+            unique_combos.append(tuple_set)
 
-        gold = [[('X', 'NH'), ('R', 'CN')], [('X', 'NC(O)OEt'), ('R', 'CN')], [('X', 'O'), ('R', 'CN')], [('X', 'O'), ('R', 'H')]]
+        gold = [{('R', 'CN'), ('X', 'NH')}, {('R', 'CN'), ('X', 'NC(O)OEt')}, {('R', 'CN'), ('X', 'O')}, {('R', 'H'), ('X', 'O')}]
         for diag in unique_combos:
             self.assertIn(diag, gold)
 
     def do_r_group_resolution(self, filename, filedir=examples_dir):
-        """ Tests the R-group detection, recognition and resoltuion"""
-
+        """ Tests the R-group detection, recognition and resoltuion (using OSRA)"""
 
         r_smiles = []
         smiles = []
